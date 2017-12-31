0643132f845d2cbd8db6868bf2da72a4489b1f93
Oswaldo Capriles <oswald.capriles46@gmail.com>
2017-12-31 18:45:43 -0400

    Fixed read and write error

--- Utils.py
+++ Utils.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3.6
+import os
 
 CHATS_PATH = 'chats.txt'
 
 def read_stored_chats():
-    with open(CHATS_PATH, 'w+') as f:
+
+    if not os.path.exists(CHATS_PATH):
+        write_stored_chats([])
+
+    with open(CHATS_PATH, 'r') as f:
         chats = f.read().splitlines()
+
     return chats
 
 def write_stored_chats(chats):
+
     with open(CHATS_PATH, 'w') as f:
-        f.writelines(chats)
+        f.write("\n".join(chats))
 
 def input_token():
     # Enter some token from input
